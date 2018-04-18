11095eace4a10b656469b60f31197d3b9a718709
jessalbesshero <speed_chucho@hotmail.com>
2018-04-18 16:26:18 -0500

    Mi app Django, primer commit

---
+++ myblogsite/blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+# Ejemplo seguido de un tutorial https://tutorial.djangogirls.org/es/django_models/
+
+admin.site.register(Post)
---
+++ myvendv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ideapad  y700  17.3\djangoboys\myvendv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
